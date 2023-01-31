df6c009d79cb94cf3afd31ed6cbaa0a2466f0a7c
Joseph Park <josephpark.jyp@gmail.com>
2023-01-31 10:55:55 -0800

    Semi-completed hamburger menu
    Bugs:
    1. {overflow: 'hidden'} on .nav hides the menu (removing horizontal scroll bar), but the hamburger button no longer works.
    - The 'hidden' class is still applied to nav-ul; the menu hasn't technically been in view yet, so the hidden class hasn't been removed.
    - Removing hidden class did not resolve the bug.
    2. If I try to hide the menu vertically (inside the home section, comes down when hamburger icon is pressed), it clips with the about section. Setting z-index does not entirely resolve the issue.

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
-window.addEventListener("scroll", function() {
-    var navbar = document.querySelector(".navbar")
-    navbar.classList.toggle("sticky", window.scrollY > window.innerHeight)
+// Sticks the navigation bar (which is located just below the home section)
+// to the top of the screen after the bar reaches the top of the page.
+// Unsticks once the home section is in view again.
+window.addEventListener('scroll', function() {
+    const nav = document.querySelector('.nav')
+    nav.classList.toggle('sticky', window.scrollY > window.innerHeight)
 })
 
-const btn_explore = document.getElementById("btn-explore")
+// Pressing the "Explore My Work" button smooth-scrolls down to the about section.
+// Note: Begun dev process with this button so insted of changing to an a tag
+// I kept it since the CSS was already well written.
+const btn_explore = document.getElementById('btn-explore')
 btn_explore.addEventListener('click', () => {
     document.getElementById('about').scrollIntoView({behavior: 'smooth'})
 })
 
-
+// Observes whether an element is viewable. If it is, fade in the element.
+// Optionally, can add an else to remove the 'show' class.
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-        console.log(entry)
         if (entry.isIntersecting) {
             entry.target.classList.add('show')
         }
@@ -21,4 +27,14 @@ const observer = new IntersectionObserver((entries) => {
 const hiddenElements = document.querySelectorAll('.hidden')
 hiddenElements.forEach((element) => {
     observer.observe(element)
+})
+
+
+
+const hamburger = document.querySelector('.hamburger')
+const nav_ul = document.querySelector('.nav-ul')
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active')
+    nav_ul.classList.toggle('active')
 })
